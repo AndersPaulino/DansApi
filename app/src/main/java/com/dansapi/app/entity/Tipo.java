40660bcbb07eefa4ad3c8308f9c79a52016d89cf
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-13 22:34:58 -0300

    Criação do atributo nomeTipo, da Classe Tipo

--- app/src/main/java/com/dansapi/app/entity/Tipo.java
+++ app/src/main/java/com/dansapi/app/entity/Tipo.java
@@ -1,9 +1,15 @@
 package com.dansapi.app.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_tipo",schema = "public")
 public class Tipo extends AbstractEntity{
+    @Getter @Setter
+    @Column(name = "cl_tipo")
+    private String nomeTipo;
 }
