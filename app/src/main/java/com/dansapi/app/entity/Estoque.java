c5da47b98eca83db5f7c61191d36a6b1e572e8c9
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-13 22:26:04 -0300

    Criação do primeiro atributo da classe Estoque, nomeEstoque

--- app/src/main/java/com/dansapi/app/entity/Estoque.java
+++ app/src/main/java/com/dansapi/app/entity/Estoque.java
@@ -1,9 +1,15 @@
 package com.dansapi.app.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_estoque", schema = "public")
 public class Estoque extends AbstractEntity{
+    @Getter @Setter
+    @Column(name = "cl_nome_estoque")
+    private String nomeEstoque;
 }
