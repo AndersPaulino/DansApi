3bc5002381e86db95c18cb7bef288a1107fd0a34
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-13 22:30:34 -0300

    Criação da terceira classe do projeto DansApi, Movimentacao, que ira tratar das movimentacoes dos produtos dentro de cada estoque

---
+++ app/src/main/java/com/dansapi/app/entity/Movimentacao.java
@@ -0,0 +1,9 @@
+package com.dansapi.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_movimentacao", schema = "public")
+public class Movimentacao extends AbstractEntity{
+}
