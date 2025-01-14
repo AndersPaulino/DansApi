65494a49e142d797d8bd3a79edc4f339ef32c88a
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-13 23:55:44 -0300

    Criação da segunda Classe DTO, MovimentacaoDTO

--- app/src/main/java/com/dansapi/app/entity/Movimentacao.java
+++ app/src/main/java/com/dansapi/app/entity/Movimentacao.java
@@ -1,9 +1,42 @@
 package com.dansapi.app.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "tb_movimentacao", schema = "public")
 public class Movimentacao extends AbstractEntity{
+    @Getter
+    @Setter
+    @OneToOne
+    @JoinColumn(name = "produto_id") // Substitua "produto_id" pelo nome da coluna real
+    private Produto produtos;
+
+
+    @Getter @Setter
+    @Column(name = "entrada")
+    private int entrada;
+
+    @Getter @Setter
+    @Column(name = "saida")
+    private int saida;
+
+    @Getter @Setter
+    @Column(name = "totalProduto")
+    private int totalProduto;
+
+    @Getter @Setter
+    @Column(name = "valorCompra", nullable = false)
+    private BigDecimal valorCompra;
+
+    @Getter @Setter
+    @Column(name = "valorVenda", nullable = false)
+    private BigDecimal valorVenda;
+
+    @Getter @Setter
+    @Column(name = "valorTotal", nullable = false)
+    private BigDecimal valorTotal;
 }
