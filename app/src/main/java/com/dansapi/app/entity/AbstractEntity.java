21563e1657fa64a1553bed2288ee2b49af83838f
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-13 22:09:01 -0300

    Criação do segundo atributo comun entre as classes, ativo

--- app/src/main/java/com/dansapi/app/entity/AbstractEntity.java
+++ app/src/main/java/com/dansapi/app/entity/AbstractEntity.java
@@ -2,6 +2,7 @@ package com.dansapi.app.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
@@ -10,4 +11,9 @@ public abstract class AbstractEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, unique = true)
     private Long id;
+
+    @Getter @Setter
+    @Column(name = "ativo", nullable = false)
+    private boolean ativo;
+
 }
