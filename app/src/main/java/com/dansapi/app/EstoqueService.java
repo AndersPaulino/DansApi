7927c6ae2f1ddd6965f9a5c759f6d0e03cfed146
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-13 23:28:39 -0300

    Criação da primeira Service do projeto DansApi

---
+++ app/src/main/java/com/dansapi/app/EstoqueService.java
@@ -0,0 +1,16 @@
+package com.dansapi.app;
+
+import com.dansapi.app.repository.EstoqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstoqueService {
+    private EstoqueRepository estoqueRepository;
+
+    @Autowired
+    public EstoqueService(EstoqueRepository estoqueRepository){
+        this.estoqueRepository = estoqueRepository;
+    }
+
+}
