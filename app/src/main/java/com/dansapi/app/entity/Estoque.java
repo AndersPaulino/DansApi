6904fd3545524a2c348f40c502348905e2c70c0b
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-13 23:51:08 -0300

    Criação da primeira Classe DTO, EstoqueDTO

--- app/src/main/java/com/dansapi/app/entity/Estoque.java
+++ app/src/main/java/com/dansapi/app/entity/Estoque.java
@@ -1,10 +1,11 @@
 package com.dansapi.app.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_estoque", schema = "public")
@@ -12,4 +13,12 @@ public class Estoque extends AbstractEntity{
     @Getter @Setter
     @Column(name = "cl_nome_estoque")
     private String nomeEstoque;
+
+    @Getter @Setter
+    @ManyToMany
+    @JoinTable(name = "cl_estoquemovimentacao",
+            joinColumns = @JoinColumn(name = "estoque_id"),
+            inverseJoinColumns = @JoinColumn(name = "movimentacao_id")
+    )
+    private List<Movimentacao> movimentacao = new ArrayList<>();
 }
