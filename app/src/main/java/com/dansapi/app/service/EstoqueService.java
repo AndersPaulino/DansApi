d92fffad05893995bffa32bcd90d20428d7e035c
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-15 18:21:19 -0300

    Criação do metodo findAll da classe EstoqueService

--- app/src/main/java/com/dansapi/app/service/EstoqueService.java
+++ app/src/main/java/com/dansapi/app/service/EstoqueService.java
@@ -1,11 +1,13 @@
 package com.dansapi.app.service;
 
 import com.dansapi.app.dto.EstoqueDTO;
+import com.dansapi.app.entity.Estoque;
 import com.dansapi.app.repository.EstoqueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,4 +23,11 @@ public class EstoqueService {
     public Optional<EstoqueDTO> findById(Long id){
         return estoqueRepository.findById(id).map(EstoqueDTO::new);
     }
+
+    @Transactional(readOnly = true)
+    public List<EstoqueDTO> findAll(){
+        List<Estoque> estoques = estoqueRepository.findAll();
+        return estoques.stream().map(EstoqueDTO::new).toList();
+    }
+
 }
