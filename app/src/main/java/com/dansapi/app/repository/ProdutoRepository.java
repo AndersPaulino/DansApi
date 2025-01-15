af87777c3b05a34fbd64ec68d441fe5f27b39bf9
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2025-01-15 18:08:45 -0300

    Criação na classe ProdutoRepository, metodos para buscas de personalizas da classe Produto

--- app/src/main/java/com/dansapi/app/repository/ProdutoRepository.java
+++ app/src/main/java/com/dansapi/app/repository/ProdutoRepository.java
@@ -2,9 +2,24 @@ package com.dansapi.app.repository;
 
 import com.dansapi.app.entity.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Long, Produto> {
+    @Query("SELECT e FROM Produto e WHERE e.nome = :nome")
+    public Produto findByNomeProduto(@Param("nome") String nome);
 
+    @Query("SELECT e FROM Produto e WHERE e.ativo = :ativo")
+    public List<Produto> findByAtivo(@Param("ativo")boolean ativo);
+
+    @Query("SELECT e FROM Produto e WHERE DATE(e.registro) = :registro")
+    List<Produto> findByDiaRegistro(@Param("registro") LocalDate registro);
+
+    @Query("SELECT e FROM Produto e WHERE DATE(e.atualizar) = :atualizar")
+    List<Produto> findByDiaAtualizar(@Param("atualizar") LocalDate atualizar);
 }
